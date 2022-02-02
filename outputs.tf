0b238c0e791f042b42879387b0ec783bae4772ba
Javier Arranz <javier.arranz@mirai-advisory.com>
2022-02-02 10:32:14 +0100

    simple is better

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "this_lambda_arn" {
   description = "The ARN of the lambda. Will be of format arn:aws:s3:::bucketname."
-  value       = element(concat(aws_lambda_function.this_function.*.arn, tolist[("")]), 0)
+  value       = aws_lambda_function.this_function.arn
 }
 
 output "this_lambda_invoke_arn" {
   description = "The invoke ARN of the lambda. Will be of format arn:aws:s3:::bucketname."
-  value       = element(concat(aws_lambda_function.this_function.*.invoke_arn, tolist[("")]), 0)
+  value       = aws_lambda_function.this_function.invoke_arn
 }
